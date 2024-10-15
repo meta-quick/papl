6434a9f135b8d04765dcc09ccdc5b93fc0b97021
gao.brian <gao.brian@gmail.com>
2024-10-15 09:48:40 +0800

    refactor: Improve parameter passing in cedar module and optimize store methods
    - Update parameter passing in `Request::new` and `add_policy` in cedar module
    - Optimize error handling and parameter passing in store methods

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rusqlite::{Connection,Result};
+use std::sync::{Arc, Mutex};
+use rusqlite::{Connection, Error, Result};
 
 pub struct SqliteStore {
-    conn: Connection,
+    conn: Arc<Mutex<Option<Connection>>>,
 }
 
 fn table_exists(conn: &Connection, table_name: &str) -> bool {
@@ -55,7 +56,7 @@ impl SqliteStore {
             )?;
         }
 
-        Ok(SqliteStore { conn })
+        Ok(SqliteStore { conn: Arc::new(Mutex::new(Some(conn))) })
     }
 
     pub fn new_in_memory() -> Result<Self> {
@@ -79,182 +80,265 @@ impl SqliteStore {
             )?;
         }
 
-        Ok(SqliteStore { conn })
+        Ok(SqliteStore { conn: Arc::new(Mutex::new(Some(conn))) })
     }
 
     pub fn save(&self, key: String, policy: String, version: String, timestamp: i64) -> Result<usize> {
-        let conn = &self.conn;
-        //update if key exists
-        let stamp = timestamp.to_string();
-        let mut stmt = conn.prepare("UPDATE policy SET policy = ?1, version = ?2, stamp = ?4  WHERE key = ?3")?;
-        let result = stmt.execute(&[&policy,&version, &key,&stamp]);
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn_lock) => {
+                //update if key exists
+                let stamp = timestamp.to_string();
+                let conn = conn_lock.as_ref().unwrap();
+                let mut stmt = conn.prepare("UPDATE policy SET policy = ?1, version = ?2, stamp = ?4  WHERE key = ?3")?;
+                let result = stmt.execute(&[&policy,&version, &key,&stamp]);
 
-        match result {
-            Ok(result) => {
-                if result > 0 {
-                    return Ok(result);
-                } else {
-                    eprintln!("Error updating policy: key: {}", key);
+                match result {
+                    Ok(result) => {
+                        if result > 0 {
+                            return Ok(result);
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Error updating policy: key: {}, {}",key, e);
+                    }
                 }
-            },
-            Err(e) => {
-                eprintln!("Error updating policy: key: {}, {}",key, e);
+
+                //insert if key does not exist
+                let mut stmt = conn.prepare("INSERT INTO policy (key,policy,version,stamp) VALUES (?1, ?2, ?3,?4)")?;
+                let result = stmt.execute(&[&key, &policy, &version,&stamp]);
+
+                match result {
+                    Ok(result) => {
+                        if result > 0 {
+                            return Ok(result);
+                        } else {
+                            eprintln!("Error inserting policy: key: {}", key);
+                            return Err(rusqlite::Error::QueryReturnedNoRows);
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Error inserting policy: key: {}, {}", key,e);
+                        Err(e)
+                    }
+                }
             }
-        }
-
-        //insert if key does not exist
-        let mut stmt = conn.prepare("INSERT INTO policy (key,policy,version,stamp) VALUES (?1, ?2, ?3,?4)")?;
-        let result = stmt.execute(&[&key, &policy, &version,&stamp]);
-
-        match result {
-            Ok(result) => {
-                if result > 0 {
-                    return Ok(result);
-                } else {
-                    eprintln!("Error inserting policy: key: {}", key);
-                    return Err(rusqlite::Error::QueryReturnedNoRows);
-                }
-            },
-            Err(e) => {
-                eprintln!("Error inserting policy: key: {}, {}", key,e);
-                return Err(e);
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
             }
         }
     }
 
     pub fn get(&self, key: String) -> Result<String> {
-        let conn = &self.conn;
-        let mut stmt = conn.prepare("SELECT policy FROM policy WHERE key = ?")?;
-        let rows = stmt.query(&[&key]);
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn_lock) => {
+                let conn = conn_lock.as_ref().unwrap();
+                let mut stmt = conn.prepare("SELECT policy FROM policy WHERE key = ?")?;
+                let rows = stmt.query(&[&key]);
 
 
-        if let Ok(mut rows) = rows {
-            if let Ok(Some(row)) = rows.next() {
-                let policy: String = row.get(0)?;
-                return Ok(policy);
+                if let Ok(mut rows) = rows {
+                    if let Ok(Some(row)) = rows.next() {
+                        let policy: String = row.get(0)?;
+                        return Ok(policy);
+                    }
+                }
+                return Err(rusqlite::Error::QueryReturnedNoRows);
+            }
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
             }
         }
-        return Err(rusqlite::Error::QueryReturnedNoRows);
     }
 
     pub fn version(&self, key: String) -> Result<String> {
-        let conn = &self.conn;
-        let mut stmt = conn.prepare("SELECT version FROM policy WHERE key = ?")?;
-        let rows = stmt.query(&[&key]);
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                let mut stmt = conn.prepare("SELECT version FROM policy WHERE key = ?")?;
+                let rows = stmt.query(&[&key]);
 
 
-        if let Ok(mut rows) = rows {
-            if let Ok(Some(row)) = rows.next() {
-                let version: String = row.get(0)?;
-                return Ok(version);
+                if let Ok(mut rows) = rows {
+                    if let Ok(Some(row)) = rows.next() {
+                        let version: String = row.get(0)?;
+                        return Ok(version);
+                    }
+                }
+                return Err(rusqlite::Error::QueryReturnedNoRows);
+            }
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
             }
         }
-        return Err(rusqlite::Error::QueryReturnedNoRows);
     }
 
     pub fn version_value(&self, key: String) -> Result<(String,String)> {
-        let conn = &self.conn;
-        let mut stmt = conn.prepare("SELECT policy,version FROM policy WHERE key = ?")?;
-        let  rows = stmt.query(&[&key]);
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                let mut stmt = conn.prepare("SELECT policy,version FROM policy WHERE key = ?")?;
+                let  rows = stmt.query(&[&key]);
 
 
-        if let Ok(mut rows) = rows {
-            if let Ok(Some(row)) = rows.next() {
-                let policy: String = row.get(0)?;
-                let version: String = row.get(1)?;
-                return Ok((policy,version));
+                if let Ok(mut rows) = rows {
+                    if let Ok(Some(row)) = rows.next() {
+                        let policy: String = row.get(0)?;
+                        let version: String = row.get(1)?;
+                        return Ok((policy,version));
+                    }
+                }
+                Err(Error::QueryReturnedNoRows)
+            }
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
             }
         }
-        return Err(rusqlite::Error::QueryReturnedNoRows);
     }
 
     pub fn all_keys_le(&self, timestamp: i64) -> Result<Vec<String>> {
-        let conn = &self.conn;
-        let stamp = timestamp.to_string();
-        let mut stmt = conn.prepare("SELECT key FROM policy WHERE stamp <= ?1")?;
-        let mut rows = stmt.query(&[&stamp])?;
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                let stamp = timestamp.to_string();
+                let mut stmt = conn.prepare("SELECT key FROM policy WHERE stamp <= ?1")?;
+                let mut rows = stmt.query(&[&stamp])?;
 
-        let mut keys = Vec::new();
-        while let Ok(Some(row)) = rows.next() {
-            let key: String = row.get(0)?;
-            keys.push(key);
+                let mut keys = Vec::new();
+                while let Ok(Some(row)) = rows.next() {
+                    let key: String = row.get(0)?;
+                    keys.push(key);
+                }
+
+                Ok(keys)
+            }
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
+            }
         }
-
-        Ok(keys)
     }
 
     pub fn all_keys_be(&self, timestamp: i64) -> Result<Vec<String>> {
-        let conn = &self.conn;
-        let stamp = timestamp.to_string();
-        let mut stmt = conn.prepare("SELECT key FROM policy WHERE stamp >= ?1")?;
-        let mut rows = stmt.query(&[&stamp])?;
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                let stamp = timestamp.to_string();
+                let mut stmt = conn.prepare("SELECT key FROM policy WHERE stamp >= ?1")?;
+                let mut rows = stmt.query(&[&stamp])?;
 
-        let mut keys = Vec::new();
-        while let Ok(Some(row)) = rows.next() {
-            let key: String = row.get(0)?;
-            keys.push(key);
+                let mut keys = Vec::new();
+                while let Ok(Some(row)) = rows.next() {
+                    let key: String = row.get(0)?;
+                    keys.push(key);
+                }
+
+                Ok(keys)
+            }
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
+            }
         }
-
-        Ok(keys)
     }
 
     pub fn all_keys_be_pageable(&self,timestamp: i64, page: i64, size: i64) -> Result<Vec<String>> {
-        let conn = &self.conn;
-        let stamp = timestamp.to_string();
-        let offset = (page - 1) * size;
-        let offset = offset.to_string();
-        if size == 0 {
-            return Ok(Vec::new());
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                let stamp = timestamp.to_string();
+                let offset = (page - 1) * size;
+                let offset = offset.to_string();
+                if size == 0 {
+                    return Ok(Vec::new());
+                }
+
+                let size = size.to_string();
+                let mut stmt = conn.prepare("SELECT key FROM policy WHERE stamp >= ?1 LIMIT ?2 OFFSET ?3")?;
+                let mut rows = stmt.query(&[&stamp,&size,&offset])?;
+
+                let mut keys = Vec::new();
+                while let Ok(Some(row)) = rows.next() {
+                    let key: String = row.get(0)?;
+                    keys.push(key);
+                }
+
+                Ok(keys)
+            }
+            Err(_) => {
+                Err(Error::SqliteSingleThreadedMode)
+            }
         }
-
-        let size = size.to_string();
-        let mut stmt = conn.prepare("SELECT key FROM policy WHERE stamp >= ?1 LIMIT ?2 OFFSET ?3")?;
-        let mut rows = stmt.query(&[&stamp,&size,&offset])?;
-
-        let mut keys = Vec::new();
-        while let Ok(Some(row)) = rows.next() {
-            let key: String = row.get(0)?;
-            keys.push(key);
-        }
-
-        Ok(keys)
     }
 
     pub fn evict_be(&self, timestamp: i64) -> Result<usize> {
-        let conn = &self.conn;
-
-        if table_exists(&conn, "policy") {
-            let stamp = timestamp.to_string();
-            let mut stmt = conn.prepare("DELETE FROM policy WHERE stamp >= ?1")?;
-            let result = stmt.execute(&[&stamp]);
-            result
-        } else {
-            Ok(0)
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                if table_exists(&conn, "policy") {
+                    let stamp = timestamp.to_string();
+                    let mut stmt = conn.prepare("DELETE FROM policy WHERE stamp >= ?1")?;
+                    let result = stmt.execute(&[&stamp]);
+                    result
+                } else {
+                    Ok(0)
+                }
+            }
+            Err(_) => {
+                return Err(Error::SqliteSingleThreadedMode)
+            }
         }
     }
 
     pub fn evict_le(&self, timestamp: i64) -> Result<usize> {
-        let conn = &self.conn;
-
-        if table_exists(&conn, "policy") {
-            let stamp = timestamp.to_string();
-            let mut stmt = conn.prepare("DELETE FROM policy WHERE stamp <= ?1")?;
-            let result = stmt.execute(&[&stamp]);
-            result
-        } else {
-            Ok(0)
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                if table_exists(&conn, "policy") {
+                    let stamp = timestamp.to_string();
+                    let mut stmt = conn.prepare("DELETE FROM policy WHERE stamp <= ?1")?;
+                    let result = stmt.execute(&[&stamp]);
+                    result
+                } else {
+                    Ok(0)
+                }
+            }
+            Err(_) => {
+                return Err(Error::SqliteSingleThreadedMode)
+            }
         }
     }
 
     pub fn delete(&self, key: String) -> Result<usize> {
-        let conn = &self.conn;
-        let mut stmt = conn.prepare("DELETE FROM policy WHERE key = ?")?;
-        let result = stmt.execute(&[&key]);
-        result
+        let lock_result = &self.conn.lock();
+        match lock_result {
+            Ok(conn) => {
+                let conn = conn.as_ref().unwrap();
+                let mut stmt = conn.prepare("DELETE FROM policy WHERE key = ?")?;
+                let result = stmt.execute(&[&key]);
+                result
+            }
+            Err(_) => {
+                return Err(Error::SqliteSingleThreadedMode)
+            }
+        }
     }
 
     pub fn close(&self) -> Result<()> {
-        let _conn = &self.conn;
+        let mut conn_guard = self.conn.lock().map_err(|_| Error::SqliteSingleThreadedMode)?;
+
+        // Take the connection out of the MutexGuard, replacing it with None
+        if let Some(conn) = conn_guard.take() {
+            // Now you can close the connection because you own it
+            conn.close().map_err(|_| Error::SqliteSingleThreadedMode)?;
+        }
+
         Ok(())
     }
 }
@@ -263,7 +347,6 @@ impl SqliteStore {
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Add;
     use super::*;
     #[test]
     fn test_sqlite_store() -> Result<()> {
@@ -311,7 +394,14 @@ mod tests {
     #[test]
     fn test_sqlite_store_delete() -> Result<()> {
         let store = SqliteStore::new("demo.db")?;
-        store.delete("key1".to_string());
+        let _ = store.delete("key1".to_string());
+        Ok(())
+    }
+
+    #[test]
+    fn test_sqlite_store_close() -> Result<()> {
+        let store = SqliteStore::new("demo.db")?;
+        store.close().unwrap();
         Ok(())
     }
 }
